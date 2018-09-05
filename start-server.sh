7a98dd12a17bff38cab9b1d80ba0baa455bdc18b
Jann Deterling <jann.deterling@smail.th-koeln.de>
2018-09-05 14:25:03 +0200

    adds src dir

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir logs
 chmod 777 logs
-python scripts/train_dialog.py && python -m rasa_core.run -d models/dialogue --enable_api --debug --endpoints config/endpoints.yaml -o ./logs/out.log
+python src/train_dialog.py && python -m rasa_core.run -d models/dialogue --enable_api --debug --endpoints config/endpoints.yaml -o ./logs/out.log
