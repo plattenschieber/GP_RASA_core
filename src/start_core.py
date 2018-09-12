eb533dd7283a90e6156d183797d613aa72f272ec
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2018-09-12 14:56:00 +0200

    enable cmd again

--- src/start_core.py
+++ src/start_core.py
@@ -1,11 +1,15 @@
 from rasa_core.interpreter import RasaNLUHttpInterpreter
 from rasa_core.agent import Agent
+from rasa_core import train
+from rasa_core.training import online
 from rasa_core import utils
 from rasa_core.run import serve_application
 import logging
 from collections import namedtuple
-import argparse
 import os
+
+logging.basicConfig()
+logger = logging.getLogger('logger')
 
 def read_endpoints(endpoint_file):
     AvailableEndpoints = namedtuple('AvailableEndpoints', 'nlg '
@@ -24,17 +28,45 @@ def read_endpoints(endpoint_file):
 
     return AvailableEndpoints(nlg, nlu, action, model)
 
-if __name__ == '__main__':
-    logger = logging.getLogger()
+def start_server(dialogue_model_path, endpoints):
     rest_api_port = int(os.environ['REST_API_PORT']) if "REST_API_PORT" in os.environ else 5005
-    dialogue_model_path = os.environ['DIALOGUE_MODEL_DIR'] if "DIALOGUE_MODEL_DIR" in os.environ else "models/dialogue"
-    enable_debug = logger.setLevel(logging.DEBUG) if "ENABLE_DEBUG" in os.environ else logger.setLevel(logging.INFO)
-    endpoints = read_endpoints(os.environ['ENDPOINTS_CONFIG_FILE']) if "DIALOGUE_MODEL_DIR" in os.environ else read_endpoints("config/endpoints.yaml")
+    server_endpoints = read_endpoints(endpoints)
+    rasaNLU = RasaNLUHttpInterpreter(project_name="default", endpoint=server_endpoints.nlu)
 
-    rasaNLU = RasaNLUHttpInterpreter(project_name="default", endpoint=endpoints.nlu)
-    logger.info("Load Agent")
     agent = Agent.load(dialogue_model_path,
                        interpreter=rasaNLU,
-                       action_endpoint=endpoints.action)
+                       action_endpoint=server_endpoints.action)
+    logger.info("Start Webserver on Port " + str(rest_api_port))
 
-    serve_application(agent, port= rest_api_port, enable_api=True)
+    channel = "cmdline"
+    if "DISABLE_CMD" in os.environ:
+        channel = None
+
+    serve_application(agent, channel=channel, port=rest_api_port, enable_api=True)
+
+def start_online_training(dialogue_model_path, endpoints):
+
+    learn_parameter = {
+        "epochs": 100,
+        "batch_size": 20,
+        "validation_split": 0.1,
+        "augmentation_factor": 50,
+        "debug_plots": False
+    }
+    agent = train.train_dialogue_model(
+        domain_file="models/dialogue/domain.yml",
+        stories_file="stories",
+        output_path=dialogue_model_path,
+        endpoints=endpoints,
+        max_history=3,
+        kwargs=learn_parameter)
+    online.serve_application(agent, serve_forever=True)
+
+if __name__ == '__main__':
+    logger.setLevel(logging.DEBUG if "ENABLE_DEBUG" in os.environ else logging.INFO)
+    dialogue_model_path = os.environ['DIALOGUE_MODEL_DIR'] if "DIALOGUE_MODEL_DIR" in os.environ else "models/dialogue"
+    endpoints = os.environ['ENDPOINTS_CONFIG_FILE'] if "DIALOGUE_MODEL_DIR" in os.environ else "config/endpoints.yaml"
+    if "ONLINE_TRAINING" in os.environ:
+        start_online_training(dialogue_model_path, endpoints)
+    else:
+        start_server(dialogue_model_path, endpoints)
