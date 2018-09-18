c7e5c544b958b789afe28c478168f7675a9182c0
Jan Seidler <jan.seidler@hotmail.com>
2018-09-18 17:00:33 +0200

    Change Bamboo Specs to test if it works fine

--- bamboo-specs/src/main/java/de/thkoeln/archilab/PlanSpec.java
+++ bamboo-specs/src/main/java/de/thkoeln/archilab/PlanSpec.java
@@ -36,7 +36,7 @@ public class PlanSpec {
             .pluginConfigurations(new ConcurrentBuilds()
                     .useSystemWideDefault(false))
             .stages(new Stage("Default Stage")
-                    .jobs(new Job("Jans Default Job",
+                    .jobs(new Job("Janssss Default Job",
                             new BambooKey("JOB1"))
                             .artifacts(new Artifact()
                                     .name("docker-compose-prod")
