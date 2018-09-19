92b7feafd82c1ebb783018324a6f00034bb640e2
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2018-09-19 14:46:03 +0200

    Update bamboo-spec

--- bamboo-spec/src/main/java/io/archi-lab/chatbot/PlanSpec.java
+++ bamboo-spec/src/main/java/io/archi-lab/chatbot/PlanSpec.java
@@ -23,72 +23,72 @@ import com.atlassian.bamboo.specs.util.BambooServer;
 
 @BambooSpec
 public class PlanSpec {
-    
+
     public Plan plan() {
         final Plan plan = new Plan(new Project()
-                .oid(new BambooOid("ky5ricqu8qv5"))
-                .key(new BambooKey("CHAT"))
-                .name("Chatbot"),
+            .oid(new BambooOid("ky5ricqu8qv5"))
+            .key(new BambooKey("CHAT"))
+            .name("Chatbot"),
             "core",
             new BambooKey("CORE"))
             .oid(new BambooOid("kxw2ardmf01t"))
             .description("Build the Rasa Core app")
             .pluginConfigurations(new ConcurrentBuilds()
-                    .useSystemWideDefault(false))
+                .useSystemWideDefault(false))
             .stages(new Stage("Default Stage")
-                    .jobs(new Job("Janssss Default Job",
-                            new BambooKey("JOB1"))
-                            .artifacts(new Artifact()
-                                    .name("docker-compose-prod")
-                                    .copyPattern("docker-compose.prod.yaml")
-                                    .location("./docker")
-                                    .shared(true)
-                                    .required(true),
-                                new Artifact()
-                                    .name("docker-compose")
-                                    .copyPattern("docker-compose.yaml")
-                                    .location("./docker")
-                                    .shared(true)
-                                    .required(true))
-                            .tasks(new VcsCheckoutTask()
-                                    .description("Checkout the repository")
-                                    .checkoutItems(new CheckoutItem().defaultRepository()),
-                                new DockerBuildImageTask()
-                                    .description("Build the Docker image")
-                                    .imageName("docker.nexus.gpchatbot.archi-lab.io/chatbot/core:latest")
-                                    .useCache(true)
-                                    .dockerfileInWorkingDir(),
-                                new DockerPushImageTask()
-                                    .customRegistryImage("docker.nexus.gpchatbot.archi-lab.io/chatbot/core:latest")
-                                    .defaultAuthentication())
-                            .dockerConfiguration(new DockerConfiguration()
-                                    .enabled(false))))
+                .jobs(new Job("Janssss Default Job",
+                    new BambooKey("JOB1"))
+                    .artifacts(new Artifact()
+                            .name("docker-compose-prod")
+                            .copyPattern("docker-compose.prod.yaml")
+                            .location("./docker")
+                            .shared(true)
+                            .required(true),
+                        new Artifact()
+                            .name("docker-compose")
+                            .copyPattern("docker-compose.yaml")
+                            .location("./docker")
+                            .shared(true)
+                            .required(true))
+                    .tasks(new VcsCheckoutTask()
+                            .description("Checkout the repository")
+                            .checkoutItems(new CheckoutItem().defaultRepository()),
+                        new DockerBuildImageTask()
+                            .description("Build the Docker image")
+                            .imageName("docker.nexus.gpchatbot.archi-lab.io/chatbot/core:latest")
+                            .useCache(true)
+                            .dockerfileInWorkingDir(),
+                        new DockerPushImageTask()
+                            .customRegistryImage("docker.nexus.gpchatbot.archi-lab.io/chatbot/core:latest")
+                            .defaultAuthentication())
+                    .dockerConfiguration(new DockerConfiguration()
+                        .enabled(false))))
             .linkedRepositories("chatbot-core (master)")
-            
+
             .triggers(new BitbucketServerTrigger())
             .planBranchManagement(new PlanBranchManagement()
-                    .delete(new BranchCleanup())
-                    .notificationForCommitters());
+                .delete(new BranchCleanup())
+                .notificationForCommitters());
         return plan;
     }
-    
+
     public PlanPermissions planPermission() {
         final PlanPermissions planPermission = new PlanPermissions(new PlanIdentifier("CHAT", "CORE"))
             .permissions(new Permissions()
-                    .userPermissions("jlengelsen", PermissionType.EDIT, PermissionType.VIEW, PermissionType.ADMIN, PermissionType.CLONE, PermissionType.BUILD)
-                    .loggedInUserPermissions(PermissionType.VIEW)
-                    .anonymousUserPermissionView());
+                .userPermissions("jlengelsen", PermissionType.EDIT, PermissionType.VIEW, PermissionType.ADMIN, PermissionType.CLONE, PermissionType.BUILD)
+                .loggedInUserPermissions(PermissionType.VIEW)
+                .anonymousUserPermissionView());
         return planPermission;
     }
-    
+
     public static void main(String... argv) {
         //By default credentials are read from the '.credentials' file.
         BambooServer bambooServer = new BambooServer("https://bamboo.gpchatbot.archi-lab.io");
         final PlanSpec planSpec = new PlanSpec();
-        
+
         final Plan plan = planSpec.plan();
         bambooServer.publish(plan);
-        
+
         final PlanPermissions planPermission = planSpec.planPermission();
         bambooServer.publish(planPermission);
     }
