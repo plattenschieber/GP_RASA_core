5748d2a2ee8d625e3a66702e4856dd5d593ada77
Jann Deterling <jann.deterling@smail.th-koeln.de>
2018-09-19 13:07:34 +0200

    adds rest api for learning mode

--- src/start_core.py
+++ src/start_core.py
@@ -1,16 +1,15 @@
 import logging
 import os
 from collections import namedtuple
-from rasa_core import train
 from rasa_core import utils
 from rasa_core.agent import Agent
-from rasa_core.channels.socketio import SocketIOInput
 from rasa_core.channels.channel import RestInput
+from rasa_core.channels.socketio import SocketIOInput
 from rasa_core.interpreter import RasaNLUHttpInterpreter
 from rasa_core.training import online
 
 logging.basicConfig()
-logger = logging.getLogger('logger')
+logger = logging.getLogger()
 
 
 def read_endpoints(endpoint_file):
@@ -55,29 +54,25 @@ def start_server(dialogue_model_path, endpoints):
     agent.handle_channels(channels, socket_port)
 
 
-
 def start_online_training(dialogue_model_path, endpoints):
-    learn_parameter = {
-        "epochs": 100,
-        "batch_size": 20,
-        "validation_split": 0.1,
-        "augmentation_factor": 50,
-        "debug_plots": False
-    }
-    agent = train.train_dialogue_model(
-        domain_file="models/dialogue/domain.yml",
-        stories_file="stories",
-        output_path=dialogue_model_path,
-        endpoints=train.AvailableEndpoints.read_endpoints(endpoints),
-        max_history=3,
-        kwargs=learn_parameter)
+    server_endpoints = read_endpoints(endpoints)
+    rasaNLU = RasaNLUHttpInterpreter(project_name="default", endpoint=server_endpoints.nlu)
+
+    agent = Agent.load(dialogue_model_path,
+                       interpreter=rasaNLU,
+                       action_endpoint=server_endpoints.action)
+
     online.run_online_learning(agent=agent)
+
 
 if __name__ == '__main__':
     logger.setLevel(logging.DEBUG if "ENABLE_DEBUG" in os.environ else logging.INFO)
     dialogue_model_path = os.environ['DIALOGUE_MODEL_DIR'] if "DIALOGUE_MODEL_DIR" in os.environ else "models/dialogue"
     endpoints = os.environ['ENDPOINTS_CONFIG_FILE'] if "DIALOGUE_MODEL_DIR" in os.environ else "config/endpoints.yaml"
+    logger.log(logging.INFO, os.environ)
     if "ONLINE_TRAINING" in os.environ:
+        logger.log(logging.INFO, "Started online trainer");
         start_online_training(dialogue_model_path, endpoints)
     else:
+        logger.log(logging.INFO, "Started start core server");
         start_server(dialogue_model_path, endpoints)
